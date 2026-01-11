452b03705628b3030a1672b546ac824c047551ac
MNR-Tushar <rahman23105101275@diu.edu.bd>
2026-01-11 23:07:49 +0600

    config settings.py and api login , register

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from .models import *
+from django.db import models
+from .managers import CustomUserManager
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(_("email address"), unique=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    first_name = models.CharField(max_length=30, null=True, blank=True)
+    last_name = models.CharField(max_length=30, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+    
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
---
+++ accounts/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import *
+
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+
+app_name = "users"
+
+urlpatterns = [
+    path("register/", UserRegisterationAPIView.as_view(), name="create-user"),
+    path("login/", UserLoginAPIView.as_view(), name="login-user"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path("logout/", UserLogoutAPIView.as_view(), name="logout-user"),
+    path("", include(router.urls)),
+
+]
